908cce3678a526387f6cc6a4c2e8a4c1d84b0493
Sweymaan <sweymaan9@gmail.com>
2025-11-17 14:54:59 +0530

    Create obd_tool.py

---
+++ src/tools/obd_tool.py
@@ -0,0 +1,9 @@
+# OBD Tool – simulated OBD-II readings
+class OBDTool:
+    def read(self):
+        return {
+            "speed": 60,
+            "rpm": 1500,
+            "tire": 30,
+            "brake_flag": 0
+        }
